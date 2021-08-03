3a06aae9c3d8701abd8131d30d31f0eb3e2e515f
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-03 10:32:55 -0400

    adding questions array, function call & working on index file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,41 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: 'username',
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: 'favoriteSong',
+        message: "What is your favorite song?"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            // Use user feedback for... whatever!!
+            console.log(answers.username)
+            console.log(answers.favoriteSong)
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
 
 // Function call to initialize app
 init();
