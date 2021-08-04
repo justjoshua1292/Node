4a3a559fb5b30049d7b71f70c10629a3ed90ae6e
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-03 21:07:04 -0400

    working on readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,7 +3,6 @@
 
 function renderLicenseBadge(license) {
 
-
   // if statements here
   // examplke: 
   // if(license == 'MIT') {}
@@ -17,6 +16,7 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+
 function renderLicenseSection(license) {}
 // ${renderLicenseBadge(answers.license)}
 
@@ -29,13 +29,13 @@ return `# ${answers.Title}
 ${answers.Description}
 
 ## Table of Contents
-- Description 
-- Installation 
-- Usage 
-- License 
-- Contributing 
-- Tests 
-- Questions 
+- Description (#Description)
+- Installation (#Installation)
+- Usage (#Usage)
+- License (#License)
+- Contributing (#Contributing)
+- Tests (#Tests)
+- Questions (#Questions)
             
 ## Installation
 ${answers.Installation}
