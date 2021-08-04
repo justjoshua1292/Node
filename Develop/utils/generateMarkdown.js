b64d0bfd9dc429886e3a54cc64aad2f28111bf9f
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-04 19:01:07 -0400

    working on generatemarkdown file, adding license rules to readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,24 +1,44 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 
-function renderLicenseBadge(license) {
+function renderLicenseBadge(license , licenseLists) {
+  if (license == licenseLists[0]){
+    var myBadge = "";
+  }else if (license == licenseLists[1]){
+    var myBadge = "";
+  }else if (license == licenseLists[2]){
+    var myBadge = "";
+  }else if (license == licenseLists[3]){
+    var myBadge = "";
+  }else if (license == licenseLists[4]){
+    var myBadge = "";
+  }
 
-  // if statements here
-  // examplke: 
-  // if(license == 'MIT') {}
-  // return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-  // else 
-}
+  return "![myBadge]"(" + myBadge + ")}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license, licenseLists) {
+  if (license == licenseLists[0]){
+    var myLicense = "" ;
+  }else if (license == licenseLists[1]){
+    var myLicense = "" ;
+  }else if (license == licenseLists[2]){
+      var myLicense = "" ;
+  }else if (license == licenseLists[3]){
+        var myLicense = "" ;
+  }else{
+    var myLicense = "";
+  }
+
+  return"[View License](" + myLicense + ")";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 
 function renderLicenseSection(license) {}
-// ${renderLicenseBadge(answers.license)}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
