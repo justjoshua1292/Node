8632743e27814c8b05974a4ed97b526a02f5c19f
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-05 21:58:42 -0400

    fixed syntax for table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -59,13 +59,13 @@ return `# ${answers.Title} 💻
 ${answers.Description}
 
 ## Table of Contents
-* [Description] (#Description) 
-* [Installation] (#Installation)
-* [Usage] (#Usage)
-* [License] (#License)
-* [Contributing] (#Contributing)
-* [Tests] (#Tests)
-* [Questions] (#Questions)
+* [Description] (##Description) 
+* [Installation] (##Installation)
+* [Usage] (##Usage)
+* [License] (##License)
+* [Contributing] (##Contributing)
+* [Tests] (##Tests)
+* [Questions] (##Questions)
             
 ## Installation 
 ${answers.Installation}
